7b7ce01503feaadd9fdec6ebf0056ad477b3ca1e
Kevin <kevin.hanna@profitisle.com>
2023-07-31 23:47:11 +0300

    added the button in the contact page

--- contact.js
+++ contact.js
@@ -45,3 +45,22 @@ gsap.to(".box1", {
 
 box1.addEventListener("mouseenter", () => animation.play());
 box1.addEventListener("mouseleave", () => animation.reverse());*/
+
+//   BUTTON
+console.clear();
+
+const buttons = gsap.utils.toArray(".button");
+buttons.forEach((item) => {
+  let span = item.querySelector("span");
+  let tl = gsap.timeline({ paused: true });
+
+  tl.to(span, {
+    duration: 0.2,
+    yPercent: -150,
+    ease: "power2.in",
+  });
+  tl.set(span, { yPercent: 150 });
+  tl.to(span, { duration: 0.2, yPercent: 0 });
+
+  item.addEventListener("mouseenter", () => tl.play(0));
+});
